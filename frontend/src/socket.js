515b8063eeb7670b7e49ad1e4135e8575b0d0195
T.Subramanyam <tsubramanyam133@gmail.com>
2026-02-03 21:56:35 +0530

    Use production socket URL

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://chitchat-xuzy.onrender.com", {
   transports: ["websocket"],
   autoConnect: true,
 });
